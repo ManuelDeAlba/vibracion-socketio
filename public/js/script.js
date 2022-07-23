65ccc4b75623ff08fb668e770d41f5f33b53a8e6
Manuel <ferman5903@hotmail.com>
2022-07-22 20:48:39 -0500

    Arreglando límite de tiempo navigator.vibrate()

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,8 @@ const socket = io();
 const btnIniciar = document.getElementById('btnIniciar');
 const btnCancelar = document.getElementById('btnCancelar');
 
+let intervalo;
+
 btnIniciar.addEventListener('click', e => {
     socket.emit("iniciar", {});
 })
@@ -12,9 +14,14 @@ btnCancelar.addEventListener('click', e => {
 })
 
 socket.on("iniciar", () => {
-    navigator.vibrate(Infinity);
+    clearInterval(intervalo);
+    navigator.vibrate(10000);
+    intervalo = setInterval(() => {
+        navigator.vibrate(10000);
+    }, 10000)
 })
 
 socket.on("cancelar", () => {
+    clearInterval(intervalo);
     navigator.vibrate(0);
 })
